9caeb1eb50e5a9c806995f673de0cfcb4f6c344f
Rafifs-ops <rafifebrians150206@gmail.com>
2025-07-02 09:51:19 +0700

    add gsap

---
+++ js/gsap.js
@@ -0,0 +1,4 @@
+gsap.from('.navbar', {duration: 1, y: -20, opacity: 0, ease: 'bounce'})
+gsap.from('.img-jumbotron-home', {duration:1 , y: -120, opacity: 0, ease: 'bounce', rotateY : 360})
+gsap.from('.title-jumbotron-home', {duration:1 , x: -80, opacity: 0, ease: 'bounce'})
+gsap.from('.sub-title-jumbotron-home', {duration:1 , y: 80, opacity: 0, ease: 'bounce'})
